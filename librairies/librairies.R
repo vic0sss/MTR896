2de65bfb174863f712a93e90f182734bbba02bf4
vic0sss <128820724+vic0sss@users.noreply.github.com>
2023-06-02 17:36:14 -0400

    Ajout d'un fichier de libraries R
    Le fichier est appelé par les autres script avec la fonction source()

---
+++ librairies/librairies.R
@@ -0,0 +1,11 @@
+## Chargement de l'ensemble des librairies nécessaires au traitement
+
+library(tidyverse)
+library(knitr)
+library(DT)
+# library(janitor)
+library(sf)
+# library(tmap)
+# library(randomcoloR)
+# library(readr)
+library(stringi)
